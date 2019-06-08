69fbad5797a710c770b85ac4f443fabf48fa2cd4
Igor Hsu <chunvenovich@mail.ru>
2019-06-08 15:45:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/chinesewikivoyage/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.lenovo.chinesewikivoyage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://zh.wikivoyage.org/");
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(myWebView.canGoBack()){
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
